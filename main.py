5a1007fbd1e67412447f404fb16c84b9b3baf20c
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2021-08-26 16:54:05 +0300

    Flask - added ability to buy a product

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from cloudipsp import Api, Checkout
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///shop.db'
@@ -16,10 +18,28 @@ class Item(db.Model):
     price = db.Column(db.Integer, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
 
+    def __repr__(self):
+        return self.title
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
+
+
+@app.route('/buy/<int:id>')
+def buy(id):
+    item = Item.query.get(id)
+
+    api = Api(merchant_id=1396424, secret_key='test')
+    checkout = Checkout(api=api)
+    data = {
+        "currency": "UAH",
+        "amount": str(item.price) + "00"
+    }
+    url = checkout.url(data).get('checkout_url')
+    return redirect(url)
 
 
 @app.route('/about')
@@ -27,9 +47,22 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/create')
+@app.route('/create', methods=['POST', 'GET'])
 def create():
-    return render_template('create.html')
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "An error happened"
+    else:
+        return render_template('create.html')
 
 
 if __name__ == "__main__":
